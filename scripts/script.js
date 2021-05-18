9bf92b0e956eb61325f20bc7d52d237d75c650f0
Ruslan Galimzyanov <ruska1812@gmail.com>
2021-05-18 20:56:45 +0500

    feature/card-grid

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,14 @@ let closePopup = document.querySelector('.popup__container_close');
 function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
-
+//выше функция для вкл/выкл поп-апа профиля
 popup.addEventListener('click', function(event) {
   if (event.target === event.currentTarget) {
     togglePopup();
   }
 })
+//а это код для закрытия поп-апа вне активного окна
+
 
 openPopup.addEventListener('click', togglePopup);
 
@@ -21,5 +23,13 @@ let input = document.querySelector('.popup__container_name');
 input.setAttribute('placeholder', name)
 
 let about = document.querySelector('.profile__info_description').textContent;
-let aboutInput = document.querySelector('.popup__container_about');
-aboutInput.setAttribute('placeholder', about);
+let input2 = document.querySelector('.popup__container_description');
+input2.setAttribute('placeholder', about);
+//выше два блока кода для авто-вставки текста со страницы в placeholder формы
+
+//ниже код для кнопки like
+let like = document.querySelector('.element__like-button');
+function likeButton() {
+  like.classList.toggle('element__like-button_active');
+}
+like.addEventListener('click', likeButton);
