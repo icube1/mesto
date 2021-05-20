506d03334d982a1bfd11c524338dbe02ed652f24
Ruslan Galimzyanov <ruska1812@gmail.com>
2021-05-20 16:55:38 +0500

    hotfix/BEM

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
-let openPopup = document.querySelector('.profile__info_edit-button');
+let openPopup = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closePopup = document.querySelector('.popup__container_close');
+let closePopup = document.querySelector('.popup__close');
 let profile = document.querySelector('.profile')
-let formElement = popup.querySelector('.popup__container_form')
-let profileName = profile.querySelector('.profile__info_name').textContent;
-let nameInput = popup.querySelector('.popup__container_name');
+let formElement = popup.querySelector('.popup__form')
+let profileName = profile.querySelector('.profile__name').textContent;
+let nameInput = popup.querySelector('.popup__name');
 
-let about = profile.querySelector('.profile__info_description').textContent;
-let jobInput = popup.querySelector('.popup__container_description');
+let about = profile.querySelector('.profile__description').textContent;
+let jobInput = popup.querySelector('.popup__description');
 
 
 
@@ -16,10 +16,10 @@ function togglePopup() {
   nameInput.setAttribute('value', profileName);
   jobInput.setAttribute('value', about);
 }
-//выше функция для вкл/выкл поп-апа профиля
+//выше функция для вкл/выкл поп-апа редактирования профиля
 
 
-popup.addEventListener('click', function(event) {     //код для закрытия поп-апа профиля вне активного окна
+popup.addEventListener('click', function(event) {     //код для закрытия поп-апа редактирования профиля вне активного окна
   if (event.target === event.currentTarget) {
     togglePopup();
   }
@@ -35,8 +35,8 @@ function formSubmitHandler (evt) { //Форма отправки новых им
   let nameValue = nameInput.value;
   let aboutValue = jobInput.value;
   // Получили значение полей jobInput и nameInput из свойства value
-  profile.querySelector('.profile__info_name').textContent = nameValue;
-  profile.querySelector('.profile__info_description').textContent = aboutValue;
+  profile.querySelector('.profile__name').textContent = nameValue;
+  profile.querySelector('.profile__description').textContent = aboutValue;
   // Вставили новые значения
   togglePopup();
 }
