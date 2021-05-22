b9eb53b27b61924154ac9b9f1bfe1eb29bb1a160
Ruslan Galimzyanov <ruska1812@gmail.com>
2021-05-22 11:21:33 +0500

    hotfix/review

--- scripts/index.js
+++ scripts/index.js
@@ -3,44 +3,46 @@ let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close');
 let profile = document.querySelector('.profile')
 let formElement = popup.querySelector('.popup__form')
-let profileName = profile.querySelector('.profile__name').textContent;
-let nameInput = popup.querySelector('.popup__name');
-
-let about = profile.querySelector('.profile__description').textContent;
-let jobInput = popup.querySelector('.popup__description');
-
+let profileName = profile.querySelector('.profile__name');
+let nameInput = popup.querySelector('.popup__form_input_name');
+let about = profile.querySelector('.profile__description');
+let jobInput = popup.querySelector('.popup__form_input_description');
 
 
-function togglePopup() {
+
+function showPopup() {
   popup.classList.toggle('popup_opened');
-  nameInput.setAttribute('value', profileName);
-  jobInput.setAttribute('value', about);
+  nameInput.value = profileName.textContent;
+  jobInput.value = about.textContent;
 }
+function hidePopup() {
+  popup.classList.toggle('popup_opened');
+}
+
 //выше функция для вкл/выкл поп-апа редактирования профиля
 
 
-popup.addEventListener('click', function(event) {     //код для закрытия поп-апа редактирования профиля вне активного окна
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-})
-
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+// popup.addEventListener('click', function (event) { //код для закрытия поп-апа редактирования профиля вне активного окна
+//   if (event.target === event.currentTarget) {
+//     togglePopup();
+//   }
+// })
 
 
 
-function formSubmitHandler (evt) { //Форма отправки новых имени и описания
+
+function formSubmitHandler(evt) { //Форма отправки новых имени и описания
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   let nameValue = nameInput.value;
   let aboutValue = jobInput.value;
   // Получили значение полей jobInput и nameInput из свойства value
-  profile.querySelector('.profile__name').textContent = nameValue;
-  profile.querySelector('.profile__description').textContent = aboutValue;
+  profileName.textContent = nameValue;
+  about.textContent = aboutValue;
   // Вставили новые значения
-  togglePopup();
+  hidePopup();
 }
 
 // Прикрепляем обработчик к форме
+openPopup.addEventListener('click', showPopup);
+closePopup.addEventListener('click', hidePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
